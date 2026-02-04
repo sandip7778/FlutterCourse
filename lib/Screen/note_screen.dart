4e87bb3f1f92bd8048661b33557320afbfecaf6e
sandip7778 <sandipydv892@gmail.com>
2026-02-04 20:44:57 +0545

    note app complete

--- lib/Screen/note_screen.dart
+++ lib/Screen/note_screen.dart
@@ -9,18 +9,104 @@ class NoteScreen extends StatefulWidget {
 
 class _NoteScreenState extends State<NoteScreen> {
 
-  TextEditingController controller = TextEditingController();
-  List<String> note = [];
+  TextEditingController titleController = TextEditingController();
+  TextEditingController contentController = TextEditingController();
+  List<NoteModel> note = [];
 
   void addItem(){
-    if(controller.text.isNotEmpty){
+    if(titleController.text.isNotEmpty && contentController.text.isNotEmpty){
       setState(() {
-        note.add(controller.text);
-        controller.clear();
+        note.add(NoteModel(title: titleController.text, content: contentController.text, createdAt: DateTime.now().toString()));
+        titleController.clear();
+        contentController.clear();
       });
     }
+  }
 
+  void deleteItem(int index){
+    setState(() {
+      note.removeAt(index);
+    });
+  }
 
+  void updateItem(int index , NoteModel notes){
+    TextEditingController titleController = TextEditingController(text: notes.title);
+    TextEditingController contentController = TextEditingController(text: notes.content);
+
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text("Update Note"),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  controller: titleController,
+                  decoration: InputDecoration(
+                      hintText: "Title"
+                  ),
+                ),
+                SizedBox(height: 20,),
+                TextField(
+                  controller: contentController,
+                  decoration: InputDecoration(
+                      hintText: "Content"
+                  ),
+                ),
+                SizedBox(height: 20,),
+                ElevatedButton(onPressed: (){
+                  setState(() {
+                   note[index].title = titleController.text;
+                   note[index].content = contentController.text;
+                  });
+                  Navigator.pop(context);
+                },child: Text("Update"))
+
+              ],
+            )
+          );
+        }
+    );
+
+  }
+
+  void addSheet(){
+    showModalBottomSheet(
+        context: context,
+        builder: (context){
+          return Container(
+            width: double.infinity,
+            height: 380,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  Text("Add Note",style: TextStyle(fontSize: 24,fontWeight: FontWeight.bold),),
+                  SizedBox(height: 20,),
+                  TextField(
+                    controller: titleController,
+                    decoration: InputDecoration(
+                      hintText: "Title"
+                    ),
+                  ),
+                  SizedBox(height: 20,),
+                  TextField(
+                    controller: contentController,
+                    decoration: InputDecoration(
+                        hintText: "Content"
+                    ),
+                  ),
+                  SizedBox(height: 20,),
+                  ElevatedButton(onPressed: (){
+                    addItem();
+                  }, child: Text("Add"))
+                ],
+              ),
+            ),
+          );
+        }
+    );
   }
 
   @override
@@ -36,40 +122,55 @@ class _NoteScreenState extends State<NoteScreen> {
             child: ListView.builder(
                 itemCount: note.length,
                 itemBuilder: (context,index){
-                  return ListTile(
-                    title: Text("${note[index]}"),
+                  final item = note[index];
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Card(
+                      color: Colors.white,
+                      child: ListTile(
+                        title: Text("${item.title}",style: TextStyle(fontSize: 22),),
+                        subtitle: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("${item.content}",),
+                            Text("${item.createdAt}",)
+                          ],
+                        ),
+                        trailing: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                          InkWell(child: Icon(Icons.edit),onTap: (){
+                            updateItem(index, item);
+                          },),
+                          SizedBox(width: 10,),
+                            InkWell(child: Icon(Icons.delete,color: Colors.redAccent,),onTap: (){
+                              deleteItem(index);
+                            },),
+                        ],),
+                      ),
+                    ),
                   );
                 }
             ),
           )
         ],
       ),
-     bottomNavigationBar: Container(
-       width: double.infinity,
-       height: 120,
-       color: Colors.red,
-       child: SafeArea(
-         child: Padding(
-           padding: const EdgeInsets.symmetric(horizontal: 16),
-           child: Row(
-             children: [
-               Expanded(child:  TextField(
-                 keyboardType: TextInputType.phone,
-                 controller: controller,
-                 decoration: InputDecoration(
-                     hintText: "Enter Note",
-                     border: InputBorder.none
-                 ),
-               ),),
-               SizedBox(width: 10,),
-               IconButton(onPressed: (){
-                 addItem();
-               }, icon: Icon(Icons.add)),
-             ],
-           ),
-         )
-       ),
-     ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          addSheet();
+        },
+        child: Icon(Icons.add,size: 32,),
+      ),
     );
   }
+}
+
+class NoteModel{
+  String title;
+  String content;
+  String createdAt;
+
+  NoteModel({required this.title,required this.content,required this.createdAt});
+
+  void operator [](int other) {}
 }
